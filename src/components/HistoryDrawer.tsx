9d06d06c17b199cf97c772895c1d2332d04e157e
arrow767 <i.silnov.anton@gmail.com>
2025-07-16 01:36:09 +0400

    mini fixes

--- src/components/HistoryDrawer.tsx
+++ src/components/HistoryDrawer.tsx
@@ -51,9 +51,6 @@ export default function HistoryDrawer({
             </Button>
           </div>
           <SheetClose asChild>
-            <Button variant="ghost" size="sm">
-              <X className="h-4 w-4" />
-            </Button>
           </SheetClose>
         </SheetHeader>
 
